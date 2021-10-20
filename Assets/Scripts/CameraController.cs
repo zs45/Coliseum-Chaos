3fe12565af6daed3773f7682005e30bb5b30101a
Zach Schrock <zschrock45@gmail.com>
2021-10-20 14:08:26 -0500

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Camera myCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCamera = gameObject.GetComponent<Camera>();
+        Debug.Log("myCamera attached to " + myCamera.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            myCamera.transform.Rotate(-0.05f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            myCamera.transform.Rotate(0.05f, 0f, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        Debug.Log("Player is attached to " + player.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            player.transform.Translate(0.01f, 0f, 0f);
+        }
+    }
+}
